7718e258a0a5869da68b15468a84ae2f4134461b
Salome Lamprecht <salomelamprecht@outlook.com>
2021-04-17 22:07:29 +0100

    fix jokes api error data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,17 @@ function getJoke() {
     .then((response) => response.json())
     .then((data) => randomJoke(data))
     .catch(function (error) {
-      alert('Oops! Something went wrong, please try again.');
-     }); 
+      alert('Oops! Something went wrong, please try again:' + error);
+    });
 }
 
 // get button that opens the jokes
-const jokesBtn = document
-  .getElementById('jokesBtn')
-  .addEventListener('click', getJoke);
+document.getElementById('jokesBtn').addEventListener('click', getJoke);
 
 // write function that is called when fetching the api data
 function randomJoke(jokeData) {
   let jokesDiv = document.getElementById('jokesDiv');
-  if (jokeData.type === "single") {
+  if (jokeData.type === 'single') {
     jokesDiv.innerHTML = `<h3 class="modal-title">${jokeData.joke}</h3>`;
   } else {
     jokesDiv.innerHTML = `<h3>${jokeData.setup}</h3>
